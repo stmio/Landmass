5918994265f840be1b3fdb89daac4ad19830dfc2
Sam Turner <98767222+stmio@users.noreply.github.com>
2022-02-12 20:58:51 +0000

    Initial commit

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MeshGenerator {
+    public static void GenerateTerrainMesh(float[,] heightMap) {
+        
+    }
+}
